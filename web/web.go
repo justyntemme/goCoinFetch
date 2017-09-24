67d0d1615da0aba3c7654e0cecc04ad6e977a4ed
Justyn Temme <Justyntemme@gmail.com>
2017-09-24 18:25:00 -0500

    Finished first functionality; the ability to get ask:bid of any cryptocoin

---
+++ web/web.go
@@ -0,0 +1,36 @@
+package web
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type ticker struct {
+	Ask string `json:"ask"`
+	Bid string `json:"bid"`
+}
+
+type coin struct {
+	CreatedOn string `json:"created_on"`
+	UnixTime  string `json:"unix_time"`
+	Cticker   ticker `json:"ticker"`
+}
+
+func grabTicker(coinHandle string) string {
+	price := ""
+
+	Ccoin := new(coin)
+
+	resp, err := http.Get("https://api.nexchange.io/en/api/v1/price/BTC/" + coinHandle + "USD/?format=json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.NewDecoder(resp.Body).Decode(&Ccoin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	price = "Ask: " + Ccoin.Cticker.Ask + " Bid: " + Ccoin.Cticker.Bid
+	return price
+}
